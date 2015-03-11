43e8af9280f7e040b2f0c14b651d363e6cb15a4d
WhatElseMode <stuartmackey11@gmail.com>
2015-03-11 18:21:02 +0000

    pushing website

---
+++ script.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+
+$(document).ready(function() {
+
+alert('You now have jQuery available to use in this page');
+
+});
+
+window.onload = function(){ 	alert("welcome"); 
+}
+
+$('#one') 
